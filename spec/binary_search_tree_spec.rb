22bdbf18d6be990e49b494c9f5c00952e03f7649
Chris Asercion <chris.asercion@gmail.com>
2024-05-23 13:17:54 -0600

    Insert

--- spec/binary_search_tree_spec.rb
+++ spec/binary_search_tree_spec.rb
@@ -12,6 +12,10 @@ RSpec.describe BinarySearchTree do
     it "insert" do
       tree = BinarySearchTree.new
       expect(tree.insert(61, "Bill & Ted's Excellent Adventure")).to eq(0)
+      expect(tree.insert(16, "Johnny English")).to eq(1)
+      expect(tree.insert(92, "Sharknado 3")).to eq(1)
+      expect(tree.insert(50, "Hannibal Buress: Animal Furnace")).to eq(2)
+
     end
   end
 end
