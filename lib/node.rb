25373077585067e88892b2cd3a29e1dc77e2151a
Kailey Kaes <kaileykaes@gmail.com>
2024-07-24 14:15:08 -0600

    comment node file

--- lib/node.rb
+++ lib/node.rb
@@ -1,6 +1,6 @@
 class Node
   attr_reader :score, :title 
-  attr_accessor :left, :right, :parent, :next_node
+  attr_accessor :left, :right, :parent, :next_node  #next_node can be written to and stored as an attribute
 
   def initialize(score, title, next_node = nil)
     @score = score
