58532250af69803842b8850b2dba3217e1596f66
Kailey Kaes <kaileykaes@gmail.com>
2024-07-25 10:00:29 -0600

    test node

---
+++ spec/node_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+RSpec.describe Node do
+  before(:each) do
+    @node = Node.new(95, 'When Harry Met Sally')
+  end
+
+  describe 'initialize' do 
+    it 'exists and has attributes' do 
+      expect(@node).to be_a Node
+
+      expect(@node.score).to eq(95)
+      expect(@node.title).to eq('When Harry Met Sally')
+      expect(@node.left).to eq(nil)
+      expect(@node.right).to eq(nil)
+      expect(@node.parent).to eq(nil)
+      expect(@node.next_node).to eq(nil)
+      expect(@node.tag).to eq(false)
+    end
+  end
+end
